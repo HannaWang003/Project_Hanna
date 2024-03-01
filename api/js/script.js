79f745103e4a3e7f08d0b8e48b878d809a6037fd
Hanna <hannawang003@gmail.com>
2024-03-01 16:35:21 +0800

    update

---
+++ api/js/script.js
@@ -0,0 +1,4 @@
+let unsplashUrl =
+  "https://api.unsplash.com/search/photos?client_id=p_FXEKINRibJ8Pbk1sCvissC6mPMnIz9VwxwKupR2ZU";
+let openweatherUrl =
+  "https://api.openweathermap.org/data/2.5/weather?appid=333bc6f6e4f76478e0c7ee7624bff50a";
